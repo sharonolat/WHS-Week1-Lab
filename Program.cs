f592de83a6b06226b459dacdd5dabb7f0b1f7881
sharonolat <sharonolatunji@gmail.com>
2022-04-03 16:18:39 +0100

    updated

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace New_Week1_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I'm Olatunji Sharon, I am in the Backend development class of Wella Schools.");
+            Console.WriteLine("I decided to learn backend development because the I believe it is the future of the tech world and a considerably good starting point to much more.");
+        }
+    }
+}
